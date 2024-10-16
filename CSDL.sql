02e48e15acb4460b1f33435a04d9c5dfc13c6ea5
Nguyễn Minh Quyền <23521325@gm.uit.edu.vn>
2024-10-16 16:49:18 +0700

    Thêm csdl của bài

---
+++ CSDL.sql
@@ -0,0 +1,19 @@
+﻿create database QuanLyNguoiDung
+go
+
+use QuanLyNguoiDung 
+go
+
+set dateformat DMY
+-- userAccount table
+
+create table UserAccount (
+	UserName nvarchar(100) primary key,
+	PassWord nvarchar(500) not null default 0,
+	FullName nvarchar(100) not null default N'User',
+	Email nvarchar(150) not null,
+	BirthDay Date not null,
+)
+go
+
+insert into UserAccount values ('admin01', 'admin01123456@', 'Nguyễn Minh Quyền', 'admin01@gmail.com', '05/11/2005')
